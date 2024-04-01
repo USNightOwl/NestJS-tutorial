d66c6f4ffb126adc6c5e8664c315dfa9a5a906b4
21120447 <haonhat2729@gmail.com>
2024-04-02 00:15:12 +0700

    authen user

---
+++ nestjs-api-app/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrkey: configService.get('JWT_TOKEN'),
+    });
+  }
+}

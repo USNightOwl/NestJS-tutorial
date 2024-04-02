7ceee37e8d97f29fc8909c7fba02f52ac69e8935
21120447 <haonhat2729@gmail.com>
2024-04-02 16:45:54 +0700

    get me with authenticate

--- nestjs-api-app/src/auth/strategy/jwt.strategy.ts
+++ nestjs-api-app/src/auth/strategy/jwt.strategy.ts
@@ -2,13 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    configService: ConfigService,
+    public prismaService: PrismaService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrkey: configService.get('JWT_TOKEN'),
+      secretOrKey: configService.get('JWT_SECRET'),
     });
+  }
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    delete user.hashedPassword;
+    return user;
   }
 }
--- nestjs-api-app/src/user/user.controller.ts
+++ nestjs-api-app/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+import { MyJwtGuard } from 'src/auth/guard';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  //@UseGuards(AuthGuard('jwt'))
+  @UseGuards(MyJwtGuard)
+  @Get('me')
+  me(@Request() request) {
+    return request.user;
+  }
+}

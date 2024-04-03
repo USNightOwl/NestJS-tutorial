733cb44ae2be851a4bae41b7b903a3df91b8ab7c
21120447 <haonhat2729@gmail.com>
2024-04-03 15:40:39 +0700

    connect postgres database

--- todo-app-nestjs-react/backend/src/app.module.ts
+++ todo-app-nestjs-react/backend/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.local.env'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD') || '',
+        synchronize: configService.get<boolean>('DB_SYNC'),
+        logging: configService.get<boolean>('DB_LOGGING'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+    }),
+    UserModule,
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

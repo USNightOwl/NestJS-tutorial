6cf48960185edffb8343fec75f472f88bb50e8ec
21120447 <haonhat2729@gmail.com>
2024-04-04 08:57:30 +0700

    login user (return accessToken)

--- ecommerce/src/users/users.controller.ts
+++ ecommerce/src/users/users.controller.ts
@@ -1,17 +1,8 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { UserSignUpDTO } from './dto/user-signup.dto';
 import { User } from './entities/user.entity';
+import { UserSignInDTO } from './dto/user-signin.dto';
 
 @Controller('users')
 export class UsersController {
@@ -22,28 +13,13 @@ export class UsersController {
     return { user: await this.usersService.signup(userSignUpDTO) };
   }
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+  @Post('signin')
+  async signin(
+    @Body() userSignInDTO: UserSignInDTO,
+  ): Promise<{ accessToken: string; user: User }> {
+    const user = await this.usersService.signin(userSignInDTO);
+    const accessToken = await this.usersService.accessToken(user);
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return { accessToken, user };
   }
 }
--- ecommerce/src/users/users.service.ts
+++ ecommerce/src/users/users.service.ts
@@ -1,11 +1,11 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { UserSignUpDTO } from './dto/user-signup.dto';
-import { hash } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
+import { UserSignInDTO } from './dto/user-signin.dto';
+import { sign } from 'jsonwebtoken';
 
 @Injectable()
 export class UsersService {
@@ -25,27 +25,31 @@ export class UsersService {
     return user;
   }
 
+  async signin(userSignInDTO: UserSignInDTO): Promise<User> {
+    const userExists = await this.userRepository
+      .createQueryBuilder('users')
+      .addSelect('users.password')
+      .where('users.email=:email', { email: userSignInDTO.email })
+      .getOne();
+    if (!userExists) throw new BadRequestException('Bad creadentials.');
+    const matchPassword = await compare(
+      userSignInDTO.password,
+      userExists.password,
+    );
+    if (!matchPassword) throw new BadRequestException('Bad creadentials.');
+    delete userExists.password;
+    return userExists;
+  }
+
   async findUserByEmail(email: string) {
     return await this.userRepository.findOneBy({ email });
   }
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async accessToken(user: User): Promise<string> {
+    return sign(
+      { id: user.id, email: user.email },
+      process.env.ACCESS_TOKEN_SECRET_KEY,
+      { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME + 'd' },
+    );
   }
 }
